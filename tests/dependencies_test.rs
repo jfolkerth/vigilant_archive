a365567ed8bc39ac460c0549fe28b0f3f20d3853
Jenzo <jennifer.folkerth@gmail.com>
2023-12-10 13:03:07 -0500

    Add htmx

--- tests/dependencies_test.rs
+++ tests/dependencies_test.rs
@@ -26,4 +26,22 @@ mod dependencies_test {
             .unwrap();
         assert_eq!("v3.3.6", response.tag_name);
     }
+
+    #[test]
+    fn using_latest_htmx_version() {
+        let key = "VIGILANT_ARCHIVE_USER_AGENT";
+        let message = &*format!(
+            "Please set the environment variable {} to run dependency tests",
+            key
+        );
+        let user_agent = env::var(key).expect(message);
+        let response: GitHubResponse = reqwest::blocking::Client::new()
+            .get("https://api.github.com/repos/bigskysoftware/htmx/releases/latest")
+            .header(USER_AGENT, user_agent)
+            .send()
+            .unwrap()
+            .json()
+            .unwrap();
+        assert_eq!("v1.9.9", response.tag_name);
+    }
 }
